71528f280b99797b89abfea72ef58ac661cd6a82
Short-cat <ivaneen@gmail.com>
2025-01-15 11:43:01 -0600

    Supreme Landscaping changes

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,6 +1,6 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
+    "name": "Supreme Landscaping & Fencing",
+    "email": "help@gmail.app",
     "phoneForTel": "555-779-4407",
     "phoneFormatted": "(555) 779-4407",
     "address": {
