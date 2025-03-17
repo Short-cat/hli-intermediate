cd16fb538e2f5c6d28e8559a186d832b6b1dcd57
Short-cat <ivaneen@gmail.com>
2025-03-17 17:14:04 -0500

    nav bar fixes & changes

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -42,5 +42,15 @@ const dropDowns = Array.from(document.querySelectorAll('#cs-navigation .cs-dropd
     item.addEventListener('click', onClick)
     }
 
+// tertiary nav toggle code
+const tertiaryDrop = Array.from(document.querySelectorAll('#cs-navigation .cs-drop3-main'));
+
+for (const item of tertiaryDrop) {
+    item.addEventListener('click', (e) => {
+        e.stopPropagation();
+        item.classList.toggle('drop3-active');
+    });
+}
+
 
                             
