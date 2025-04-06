60c101b8b7242dc04e387600c580a6c66494889b
Short-cat <ivaneen@gmail.com>
2025-04-06 14:23:39 -0500

    changes

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,22 +1,53 @@
 module.exports = {
-    "name": "Supreme Landscaping & Fencing",
+    "name": "Houston Landscape Images",
     "email": "help@gmail.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
-    "founder": "Isaac Artavia",
+    "phoneForTel": "832-545-9435",
+    "phoneFormatted": "(832) 545-9435",
+    "founder": "Sanson Faltine",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
+        "city": "Houston",
+        "state": "TX",
+        "zip": "77054",
         "country": "US",
-        "mapLink": "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6"
+        "mapLink": "https://www.google.com/maps/place/Houston+Landscape+Images/@29.6580284,-95.7479361,17z/data=!3m1!4b1!4m6!3m5!1s0x86411fb08a560d31:0xfcea71026d38ed74!8m2!3d29.6580284!4d-95.7479361!16s%2Fg%2F11fn9p8rfy?entry=ttu&g_ep=EgoyMDI1MDQwMi4xIKXMDSoJLDEwMjExNDUzSAFQAw%3D%3D"
     },
     "socials": {
-        "facebook": "https://www.facebook.com/",
-        "instagram": "https://www.instagram.com/",
+        "facebook": "https://www.facebook.com/HoustonLandscapeImages/",
+        "instagram": "https://www.instagram.com/houston_landscape_images/",
+    },
+
+    "openingHours": "Mo,Tu,We,Th,Fr,Sa,Su 06:00-19:00",
+    "areaServed": [
+        "Arcola",
+        "Bellaire",
+        "Brookshire",
+        "Bunker Hill Village",
+        "Cypress",
+        "Four Corners",
+        "Fresno",
+        "Fulshear",
+        "Great Wood",
+        "Hunters Creek Village",
+        "Jersey Village",
+        "Katy",
+        "Pearland",
+        "Pecan Grove",
+        "Piney Point Village",
+        "Richmond",
+        "Rosenberg",
+        "Sienna",
+        "Southside Place",
+        "Spring",
+        "Sugar Land",
+        "The Woodlands",
+        "Waller",
+        "West University Place"
+    ],
+    "aggregateRating": {
+        "@type": "AggregateRating",
+        "ratingValue": "5",
+        "reviewCount": "49"
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    "domain": "https://houstonlandscapeimages.com"
 };
